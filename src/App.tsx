551d3734464202f3aecfb5baed06894050dce899
Gbolahan Oladeji <31252159+mo-gbolahan@users.noreply.github.com>
2023-08-21 14:23:02 +0100

    Header completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
+const App: React.FC = () => {
  
   return (
-    <>
-      hello
-
-    </>
+    <div className="App">
+    <span className="heading">Task</span>
+      
+    </div>
   )
 }
 
