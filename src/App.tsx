467494280b2eb927a3c19ff615167e553108c8d0
Gbolahan Oladeji <31252159+mo-gbolahan@users.noreply.github.com>
2023-08-23 10:28:31 +0100

    input state is set

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
+import { useState } from 'react'
 import './App.css'
 import InputField from './components/InputField/inputField'
 
 const App: React.FC = () => {
  
+  const [todo, setTodo] =useState<string>('');
+
   return (
     <div className="App">
     <span className="heading">Task</span>
-      <InputField/>
+      <InputField todo={todo} setTodo={setTodo} />
     </div>
   )
 }
--- src/components/InputField/inputField.tsx
+++ src/components/InputField/inputField.tsx
@@ -1,9 +1,14 @@
 import "./inputfield.style.css";
 
-const InputField = () => {
+interface Props {
+  todo: string,
+  setTodo: React.Dispatch<React.SetStateAction<string>>,
+}
+
+const InputField = ({todo, setTodo}: Props) => {
   return (
     <form className="input">
-      <input type='text' placeholder='Enter a task' className='input__box'/>
+      <input type='text' placeholder='Enter a task' value={todo}  onChange={e => setTodo(e.target.value)}className='input__box'/>
       <button type='submit' className='input__submit'>Go</button>
     </form >
   )
